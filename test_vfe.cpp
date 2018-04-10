bca7b12336047bed1ba14894f0ce78997c2af90c
alaya <lvwenlong_lambda@qq.com>
2018-04-10 19:22:20 +0800

    Update

--- test_vfe.cpp
+++ test_vfe.cpp
@@ -37,7 +37,6 @@ int main(int arg_num, char** args)
     gp.set_inducing(train_x.rightCols(num_inducing));
     VectorXd init_hyp           = gp.get_default_hyps();
     init_hyp(init_hyp.size()-2) = log(0.5*stddev<VectorXd>(train_y));
-    init_hyp                    = gp.select_init_hyp(500, init_hyp);
     auto t1                     = chrono::high_resolution_clock::now();
     double nlz                  = gp.train(init_hyp);
     auto t2                     = chrono::high_resolution_clock::now();
